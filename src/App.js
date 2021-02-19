246d8fbbc9d71da2334b3436f8e4748c5aacd962
Kasey Cushing <kasey.cushing@gmail.com>
2021-02-19 16:16:22 -0500

    API call gets 10 users, fills Table (Fname, age, email, phone)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Component } from "react";
+import Table from "./components/Table";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    people: [],
+  };
+
+  componentDidMount() {
+    console.log("component did mount");
+    const data = fetch("https://randomuser.me/api/?results=10");
+    data
+      .then((resp) => resp.json())
+      .then((response) => {
+        this.setState({ people: response.results });
+        console.log("setState", this.state.people);
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.people.map((person) => {
+          return (
+            <Table
+              name={person.name.first}
+              age={person.dob.age}
+              email={person.email}
+              phone={person.cell}
+              key={person.cell}
+            />
+          );
+        })}
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/components/Table.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default function Table(props) {
+  //take in generated info
+  //fill table
+  //let fullName = props.name.first + " " + props.name.last
+
+  return (
+    <div className="table">
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Age</th>
+            <th>email</th>
+            <th>Phone</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{props.name}</td>
+            <td>{props.age}</td>
+            <td>{props.email}</td>
+            <td>{props.phone}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
